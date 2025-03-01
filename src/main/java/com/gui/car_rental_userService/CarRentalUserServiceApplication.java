006bc7d4d1fc91247850fa1337cb9f01447fe2ef
Guilherme Vale <guilherme.vale98@gmail.com>
2025-03-01 14:09:28 -0300

    start user service

---
+++ src/main/java/com/gui/car_rental_userService/CarRentalUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gui.car_rental_userService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gui/car_rental_userService/CarRentalUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gui.car_rental_userService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
